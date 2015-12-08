dbaaf862aed7c5166fb99c7d77aa1876c362a08d
fanaticize <protos1000@naver.com>
2015-12-08 16:26:24 +0900

    주석추가

--- subway/PathHandler.cpp
+++ subway/PathHandler.cpp
@@ -11,9 +11,13 @@ PathHandler::~PathHandler() {
 	delete(lineList);
 }
 
-
+//경로찾는 함수1(경유역없음)
 void PathHandler::findShortestPath(string startStation, string endStation) {
+	//먼저 한다.
+
+	//최소시간 경로를 검색으로 초기화
 	initPath(startStation, MINIMUM_TIME);
+	//최소시간 경로검색
 	PathNode* shortestPath = findPath(endStation);
 
 	int totalSec = shortestPath->getTotaltime();
@@ -22,13 +26,17 @@ void PathHandler::findShortestPath(string startStation, string endStation) {
 	printTime(totalSec);
 	cout << " /환승: " << shortestPath->getTotaltransit();
 	shortestPath = reversePath(shortestPath);
-	cout << " /요금: " << calCharge(startStation, endStation, shortestPath) << "원" << endl;
+	cout << " /요금: " << /*돈계산*/calCharge(startStation, endStation, shortestPath) << "원" << endl;
 	cout << endl;
+	//경로 출력함수
 	printPath(shortestPath);
 	cout << endl;
 
+	//최소환승 경로 검색으로 다시 초기화
 	initPath(startStation, MINIMUM_TRANSIT);
+	//최소환승 경로검색
 	shortestPath = findPath(endStation);
+
 	totalSec = shortestPath->getTotaltime();
 	cout << "최소환승경로검색----" << startStation << "~" << endStation << endl;
 	cout << "시간: ";
@@ -37,13 +45,18 @@ void PathHandler::findShortestPath(string startStation, string endStation) {
 	shortestPath = reversePath(shortestPath);
 	cout << " /요금: " << calCharge(startStation, endStation, shortestPath) << "원" << endl;
 	cout << endl;
+	//경로출력함수
 	printPath(shortestPath);
 	cout << endl;	
 
+	//메모리 초기화
 	deletePathNode();
 }
 
+//경로찾기함수2 (경유역있음)
 void  PathHandler::findShortestPath(string startStation, string endStation, string midStation) {
+	//경유역의 경우엔 시작역->경유역의 경로와 경유역->도착역의 경로를 합치는식으로 경로를 구한다.
+
 	PathNode* shortestPath1;
 	PathNode* shortestPath2;
 	PathNode* shortestTransitPath1;
@@ -55,6 +68,7 @@ void  PathHandler::findShortestPath(string startStation, string endStation, stri
 	int transitTimeAtMidShrt = 0;
 	int transitTimeAtMidMin = 0;
 
+	
 	initPath(startStation, MINIMUM_TIME);
 	shortestPath1 = findPath(midStation);
 	totalTimeShrt += shortestPath1->getTotaltime();
@@ -75,6 +89,7 @@ void  PathHandler::findShortestPath(string startStation, string endStation, stri
 	totalTimeMin += shortestTransitPath2->getTotaltime();
 	totalTransitMin += shortestTransitPath2->getTotaltransit();
 
+	//경유역에서 환승하는지 여부 체크
 	transitTimeAtMidShrt = chkTransitAtMid(shortestPath1, shortestPath2);
 	transitTimeAtMidMin = chkTransitAtMid(shortestTransitPath1, shortestTransitPath2);
 	if (transitTimeAtMidShrt != 0) {
@@ -98,6 +113,7 @@ void  PathHandler::findShortestPath(string startStation, string endStation, stri
 	cout << " /요금: " << calCharge(startStation, endStation, shortestPath1, shortestPath2) << "원" << endl;
 	printPath(shortestPath1);
 	cout << "경유: " << midStation << endl;;
+	//경유역에서 환승을 할경우엔 따로 그 환승시간을 추력
 	if (transitTimeAtMidShrt != 0) {
 		cout << "환승: ";
 		printMinuteTime(transitTimeAtMidShrt);
@@ -125,6 +141,7 @@ void  PathHandler::findShortestPath(string startStation, string endStation, stri
 	deletePathNode();
 }
 
+//두 경로 사이의 경유역에서 환승이 존재하는지 체크
 int PathHandler::chkTransitAtMid(PathNode* node1, PathNode* node2) {
 	PathNode* tempNode2 = node2;
 	while (tempNode2->getParentsNode() != NULL)
@@ -147,6 +164,7 @@ int PathHandler::chkTransitAtMid(PathNode* node1, PathNode* node2) {
 	return 0;
 }
 
+//시간계산 (시분초)
 void PathHandler::printTime(int totalSec) {
 	int hour = totalSec / 3600;
 	int min = (totalSec % 3600) / 60;
@@ -167,6 +185,7 @@ void PathHandler::printTime(int totalSec) {
 		cout << "0초";
 }
 
+//시간계산(분초)
 void PathHandler::printMinuteTime(int totalSec) {
 	int min = totalSec / 60;
 	int sec = totalSec % 60;
@@ -181,10 +200,12 @@ void PathHandler::printMinuteTime(int totalSec) {
 		cout << "0초";
 }
 
+//힙초기화
 void PathHandler::resetHeap() {
 	while (!heap.empty()) heap.pop();
 }
 
+//경로검색에쓴 node들 메모리 해제
 void PathHandler::deletePathNode() {
 	PathNode* tempNode = NULL;
 	while (!nodeList.empty()) {
@@ -195,6 +216,9 @@ void PathHandler::deletePathNode() {
 	return;
 }
 
+//요금계산하는 함수 
+//요금계산의 경우 내가 구한 경로에서 신분당선과 경전철을 탔을때의 추가요금을 계산한뒤
+//시작역과 종착역의 최단경로를 다시 계산하여 그 경로의 요금을 계산하고 위의 추가요금과 합산하여 최종요금을 계산한다
 int PathHandler::calCharge(string startStation, string endStation, PathNode* node1, PathNode* node2) {
 	int inRange = 0;
 	int outRange = 0;
@@ -206,7 +230,9 @@ int PathHandler::calCharge(string startStation, string endStation, PathNode* nod
 	PathNode* tempPath = NULL;
 	Edge* tempEdge;
 	string lineName = "";
-	tempPath = node1;
+
+	//경로 1과 경로 2에서 추가요금이 들어가는 구간을 지나는지 체크
+	tempPath = node1;	
 	while (tempPath != NULL) {
 		tempEdge = tempPath->getEdge();
 		if (tempEdge != NULL) {
@@ -235,6 +261,7 @@ int PathHandler::calCharge(string startStation, string endStation, PathNode* nod
 		tempPath = tempPath->getParentsNode();
 	}
 
+	//최소거리 경로를 계산한뒤 그 경로를 기준으로 거리를 구함
 	initPath(startStation, MINIMUM_DISTANCE);
 	tempPath= findPath(endStation);
 	tempPath = reversePath(tempPath);
@@ -249,6 +276,8 @@ int PathHandler::calCharge(string startStation, string endStation, PathNode* nod
 		}
 		tempPath = tempPath->getParentsNode();
 	}
+
+	//거리와 bool 변수들을 토대로 요금계산
 	if (inRange == 0 || outRange == 0) {
 		totalRange = inRange + outRange;
 		totalRange -= 10000;
@@ -289,10 +318,12 @@ int PathHandler::calCharge(string startStation, string endStation, PathNode* nod
 	return totalCharge;
 }
 
+//올림 나눗셈
 int PathHandler::divideUpper(int a, int b) {
 	return a%b == 0 ? a / b : a / b + 1;
 }
 
+//출력함수
 void PathHandler::printPath(PathNode* node) {
 	PathNode* temp = node;
 	PathNode* lastnode;
@@ -337,6 +368,7 @@ void PathHandler::printPath(PathNode* node) {
 	cout << endl;
 }
 
+//경로의 link 방향들을 모두 뒤집는 함수
 PathNode* PathHandler::reversePath(PathNode* path) {
 	PathNode* temp = path;
 	PathNode* trail;
@@ -351,7 +383,9 @@ PathNode* PathHandler::reversePath(PathNode* path) {
 	return middle;
 }
 
-void PathHandler::initPath(string startStation, int mode) {
+//경로 초기화, 해당하는 역을 역 이름을 이용하여 찾아서 다익스트라 알고리즘에서 쓰는 힙 출발지를 초기값으로 넣어준다.
+//또한 해당 검색에서 경로를 찾을떄 쓸 우선순위를 초기값으로 설정한다(최소시간, 최소환승, 최단거리 셋중하나)
+void PathHandler::initPath(string startStation, int mode/*우선순위*/) {
 	visitList = new bool[stationCnt];
 	for (int i = 0; i < stationCnt; i++)
 		visitList[i] = false;
@@ -370,6 +404,7 @@ void PathHandler::initPath(string startStation, int mode) {
 	}
 }
 
+//경로계산, 다익스트라 알고리즘 사용
 PathNode* PathHandler::findPath(string endStation) {
 	Edge* tempEdge;
 	PathNode* tempPathNode;
@@ -383,18 +418,26 @@ PathNode* PathHandler::findPath(string endStation) {
 		if (heap.empty())
 			break;
 
+		//힘에서 가장 작은(우선순위가 높은) 노드를 pop
 		tempPathNode = heap.top();
 		heap.pop();
+
+		//해당노드가 도착지이면 return
 		if (tempPathNode->getStation()->getName() == endStation) {
 			resetHeap();
 			delete(visitList);
 			visitList = NULL;
 			return tempPathNode;
 		}
+
+		//이미 방문한 노드면 무시
 		if (visitList[tempPathNode->getStation()->getStationSeq()])
 			continue;
 		else {
+			//방문flag true로 전환
 			visitList[tempPathNode->getStation()->getStationSeq()] = true;
+
+			//방문한 노드의 edge들을 조사
 			for (vector<Edge* >::iterator IterPos0 = tempPathNode->getStation()->getEdgeList()->begin(); IterPos0 != tempPathNode->getStation()->getEdgeList()->end(); ++IterPos0) {
 				tempEdge = (*IterPos0);
 				if (tempEdge->getTo() != tempPathNode->getStation()) {
@@ -414,9 +457,12 @@ PathNode* PathHandler::findPath(string endStation) {
 					//거리계산
 					tempTotalDistance = tempPathNode->getTotaldistance() + tempEdge->getDistance();
 
+					//계산한 노드를 heap에 push(원래는 해당 경로가 가장 작을때만 push이지만 어차피 heap에서 나올때는 가장작은값만 나오기 떄문에 상관이 없음(priority queue))
 					insPathNode = new PathNode(tempPathNode, tempEdge->getTo(), tempEdge, tempPathNode->getMode(), tempTotalTime, tempTotalTransit, tempTotalDistance);
-					nodeList.push_back(insPathNode);
 					heap.push(insPathNode);
+
+					//나중에 반환하기 위해 따로 또 저장
+					nodeList.push_back(insPathNode);
 				}
 			}
 		}
@@ -425,6 +471,7 @@ PathNode* PathHandler::findPath(string endStation) {
 	return NULL;
 }
 
+//이름으로 호선 찾기
 Line* PathHandler::findLineByName(string lineName) {
 	for (vector< Line* >::iterator IterPos0 = lineList->begin(); IterPos0 != lineList->end(); ++IterPos0) {
 		if ((*IterPos0)->getName() == lineName)
@@ -433,6 +480,7 @@ Line* PathHandler::findLineByName(string lineName) {
 	return NULL;
 }
 
+//이름으로 호선에서 역 찾기
 Station* PathHandler::findStationByName(string stationName, Line* line) {
 	for (vector< Station* >::iterator IterPos0 = line->getStationList()->begin(); IterPos0 != line->getStationList()->end(); ++IterPos0) {
 		if ((*IterPos0)->getName() == stationName) {
@@ -442,7 +490,10 @@ Station* PathHandler::findStationByName(string stationName, Line* line) {
 	return NULL;
 }
 
+//DB에서 초기값들을 불러옴(node간 간격및 연결정보, 환승정보)
 void PathHandler::setLineList(SQLiteHandler* sqliteHandler) {
+
+	//line(호선), node(역), edge(간선) 정보를 불러옴
 	char* sql = "select line, current_station, next_station, weight, wait_time, distance, isMetropolitan from edge order by line, current_station, next_station";
 	sqliteHandler->select(sql);
 
@@ -489,6 +540,7 @@ void PathHandler::setLineList(SQLiteHandler* sqliteHandler) {
 	}
 	sqliteHandler->finalizeStatement();
 
+	//환승역(edge)정보 불러옴
 	sql = "select line_from, station, line_to, weight from transit order by line_from, station, line_to";
 	sqliteHandler->select(sql);
 	tempLine = NULL;
@@ -517,6 +569,7 @@ void PathHandler::setLineList(SQLiteHandler* sqliteHandler) {
 	connectToStation();
 }
 
+//간선들의 to station 부분 연결
 void PathHandler::connectToStation() {
 	Line* tempLine;
 	Line* tempToLine;
@@ -548,6 +601,7 @@ void PathHandler::connectToStation() {
 	}
 }
 
+//사용자로부터 역이름을 받았을때 존재하는 역인지 체크
 bool PathHandler::isExistStation(string name) {
 	Station* tempStation = NULL;
 	for (vector< Line* >::iterator IterPos0 = lineList->begin(); IterPos0 != lineList->end(); ++IterPos0) {
